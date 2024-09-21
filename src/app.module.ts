ea7ec104a9040784a439c8f98a70f116bc2b2b5f
PasinPeat <pasin.lawn1995@gmail.com>
2024-09-21 14:50:44 +0700

    feat:connext to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:example@127.0.0.1:27017/meowshop?authSource=admin',
+    ),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
